6738036861fe39da3d9eec27134c2e9f20b0959a
Jehan <jehan.tremback@gmail.com>
2013-10-29 16:52:46 -0700

    first try, lets see if it works

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var express = require('express')
+  , sendgrid = require('sendgrid')(
+      process.env.SENDGRID_USERNAME
+    , process.env.SENDGRID_PASSWORD
+  )
+;
+
+var app = express();
+
+app.use(express.bodyParser());
+
+app.post('/', function(req) {
+  var req_body = req.body
+    , email_body = ''
+  ;
+
+  for (var field in req_body) {
+    email_body += field + ': ';
+    email_body += req_body[field] + ', ';
+  }
+
+  // Send the email.
+  sendgrid.send({
+      to: process.env.EMAIL_TO
+    , from: process.env.EMAIL_FROM
+    , subject: process.env.EMAIL_SUBJECT
+    , text: email_body
+  }, function(err, json) {
+    if (err) { return console.error(err); }
+    console.log(json);
+  });
+});
+
+app.listen(8080);
+console.log('Express started on port 8080');
+
+
